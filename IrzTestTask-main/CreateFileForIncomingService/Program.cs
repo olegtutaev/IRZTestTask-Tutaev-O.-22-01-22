60436e9e4a678fe84b541e5cf81599453384bd0b
olegtutaev <oleg.tutaeff@yandex.ru>
2022-01-27 18:55:09 +0400

    Update Program.cs 27-01-22

--- IrzTestTask-main/CreateFileForIncomingService/Program.cs
+++ IrzTestTask-main/CreateFileForIncomingService/Program.cs
@@ -34,73 +34,82 @@ namespace CreateFileForIncomingService
                 },
             };
 
-        public static string postXMLData(string destinationUrl, string xml)
+        static void Main()
+        {
+            var path = ConfigurationManager.AppSettings["PathForFilesIncomingService"];
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+
+                IExampleDocumentForCheckGeter exampleDocumentForCheckGeter = new FakeExampleDocumentForCheckGeter();
+
+                var subjectAreaSet = new HashSet<SubjectArea> { SubjectArea.D01, SubjectArea.Tmc, SubjectArea.Bank };
+                foreach (var subjectArea in subjectAreaSet)
+                {
+                    var subjectAreaPath = Path.Combine(path, subjectArea.ToString());
+                    Directory.CreateDirectory(subjectAreaPath);
+
+                    var documents = subjectAreaDocumentComaration[subjectArea];
+                    foreach (var document in documents)
+                    {
+                        var documentPath = Path.Combine(subjectAreaPath, document.ToString());
+                        Directory.CreateDirectory(documentPath);
+
+                        var packages = exampleDocumentForCheckGeter.Get(document, 10);
+                        foreach (var package in packages)
+                        {
+                            var filePath = Path.Combine(documentPath, package.Id + ".xml");
+                            File.WriteAllText(filePath, package.Text);
+                        }
+                    }
+                }
+            }
+
+            string[] xmlPaths = Directory.GetFiles(@"..//..//..//..//IrzTestTask-main\IrzTestFile\", "*.xml", SearchOption.AllDirectories);
+
+            foreach (var item in xmlPaths)
+            {
+                string fileName = Path.GetFileName(item);
+                Console.Write(fileName + " ");
+                var xml = XDocument.Load(item);
+                PostXMLData("http://localhost:63524/api/Incoming", xml.ToString());
+            }
+        }
+
+        private static void PostXMLData(string destinationUrl, string xml)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(destinationUrl);
             byte[] bytes;
-            bytes = System.Text.Encoding.ASCII.GetBytes(xml);
+            bytes = System.Text.Encoding.UTF32.GetBytes(xml);
+            Stream requestStream = GetRequestStream(request, bytes);
+            requestStream.Write(bytes, 0, bytes.Length);
+            requestStream.Close();
+            HttpWebResponse response;
+            
+            try 
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                    string responseStr = new StreamReader(responseStream).ReadToEnd();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("200 OK");
+                Console.ResetColor();
+            }
+            catch (WebException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("400 ERROR");
+                Console.ResetColor(); 
+            }
+        }
+
+        private static Stream GetRequestStream(HttpWebRequest request, byte[] bytes)
+        {
             request.ContentType = "text/xml; encoding='utf-8'";
             request.ContentLength = bytes.Length;
             request.Method = "POST";
             Stream requestStream = request.GetRequestStream();
-            requestStream.Write(bytes, 0, bytes.Length);
-            requestStream.Close();
-            HttpWebResponse response;
-            response = (HttpWebResponse)request.GetResponse(); // В случае 400 в консоли выдаст System.Net.WebException
-
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Stream responseStream = response.GetResponseStream();
-                string responseStr = new StreamReader(responseStream).ReadToEnd();
-                Console.WriteLine("200 OK");
-                return responseStr;
-            }
-            else
-                return null;
-        }
-
-        static void Main()
-        {
-            List<string> list = new List<string>();
-
-            foreach (var file in Directory.GetFiles(@"..//..//..//..//IrzTestTask-main\IrzTestFile\", "*.xml", SearchOption.AllDirectories))
-                list.Add(file);
-
-            for (int i=0; i < list.Count; i++)
-            {
-                Console.Write(list[i] + " returned: ");
-                var xml = XDocument.Load(list[i]);
-                postXMLData("http://localhost:63524//api/Incoming", xml.ToString());
-            }
-
-            var path = ConfigurationManager.AppSettings["PathForFilesIncomingService"];
-            if(Directory.Exists(path))
-                Directory.Delete(path, true);
-            Directory.CreateDirectory(path);
-
-            IExampleDocumentForCheckGeter exampleDocumentForCheckGeter = new FakeExampleDocumentForCheckGeter();
-            
-            var subjectAreaSet = new HashSet<SubjectArea> { SubjectArea.D01, SubjectArea.Tmc, SubjectArea.Bank };
-            foreach (var subjectArea in subjectAreaSet)
-            {
-                var subjectAreaPath = Path.Combine(path, subjectArea.ToString());
-                Directory.CreateDirectory(subjectAreaPath);
-
-                var documents = subjectAreaDocumentComaration[subjectArea];
-
-                foreach (var document in documents)
-                {
-                    var documentPath = Path.Combine(subjectAreaPath, document.ToString());
-                    Directory.CreateDirectory(documentPath);
-
-                    var packages = exampleDocumentForCheckGeter.Get(document, 10);
-                    foreach (var package in packages)
-                    {
-                        var filePath = Path.Combine(documentPath, package.Id + ".xml");
-                        File.WriteAllText(filePath, package.Text);
-                    }
-                }
-            }
+            return requestStream;
         }
     }
 }
